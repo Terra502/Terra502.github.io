7b618ac0e290f509667697440fc23f0c66dca7e9
Leon Stüve <terra502tv@gmail.com>
2024-11-15 18:54:41 +0100

    Farbe

--- Web_mine/script.js
+++ Web_mine/script.js
@@ -5,16 +5,13 @@ function buildField() {
         tbl = document.createElement('table');
   tbl.style.width = '100px';
   tbl.style.border = '1px solid black';
-  //const para = document.createElement("p");
-  //const node = document.createTextNode("Test Para");
-  //para.appendChild(node);
-  //document.getElementById("main").appendChild(para);
 
   for (let i = 0; i < y; i++){
     const tr = tbl.insertRow();
     for (let j = 0; j < x; j++){
       const td = tr.insertCell();
       td.appendChild(document.createElement('button'));
+      td.style.backgroundColor = "red";
     }
   }
   body.appendChild(tbl);
