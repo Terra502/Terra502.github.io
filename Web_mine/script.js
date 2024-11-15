c0a0c30ff777632acffc2f99b18899697876ae73
Leon Stüve <terra502tv@gmail.com>
2024-11-15 18:48:30 +0100

    First Table

--- Web_mine/script.js
+++ Web_mine/script.js
@@ -1,7 +1,22 @@
 function buildField() {
-  const para = document.createElement("p");
-  const node = document.createTextNode("Test Para");
+  const x = 10;
+  const y = 10;
+  const body = document.body,
+        tbl = document.createElement('table');
+  tbl.style.width = '100px';
+  tbl.style.border = '1px solid black';
+  //const para = document.createElement("p");
+  //const node = document.createTextNode("Test Para");
+  //para.appendChild(node);
+  //document.getElementById("main").appendChild(para);
 
-  para.appendChild(node);
-  document.getElementById("main").appendChild(para);
+  for (let i = 0; i < y; i++){
+    const tr = tbl.insertRow();
+    for (let j = 0; j < x; j++){
+      const td = tr.insertCell();
+      td.appendChild(document.createTextNode('${i};${j}'));
+      td.style.border = '1px solid black';
+    }
+  }
+  body.appendChild(tbl);
 }
